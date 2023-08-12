d09c9c8f366dd136607115c5c11e4ce6eb33754b
sandeshjadhav5 <jadhavs932@gmail.com>
2023-08-12 23:59:29 +0530

    added template selection

--- client/src/Pages/Templates.tsx
+++ client/src/Pages/Templates.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Template1 from "../Components/Template1";
+import Template2 from "../Components/Template2";
+import Navbar from "../Components/Navbar";
+import { Heading, Box, Text } from "@chakra-ui/react";
 
 const Templates = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Heading mt="120px">List of Available Invoice Templates</Heading>
+      <Box border="solid #ccc 2px" m="8" p="4">
+        <Text fontWeight="40">Template 1</Text>
+        <Template1 />
+      </Box>
+      <Box border="solid #ccc 2px" m="8" p="4">
+        <Text fontWeight="40">Template 2</Text>
+        <Template2 />
+      </Box>
+    </div>
+  );
 };
 
 export default Templates;
